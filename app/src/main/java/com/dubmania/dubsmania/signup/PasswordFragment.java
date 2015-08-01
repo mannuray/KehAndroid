754395440f31a9a9a02d913c86ddd6496e64abc4
hardy047 <hardikp.001@gmail.com>
2015-08-01 20:32:55 +0530

    RestClient Created

--- app/src/main/java/com/dubmania/dubsmania/signup/PasswordFragment.java
+++ app/src/main/java/com/dubmania/dubsmania/signup/PasswordFragment.java
@@ -7,25 +7,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.dubmania.dubsmania.R;
+import com.dubmania.dubsmania.restclient.RequestListener;
+import com.dubmania.dubsmania.restclient.RestClient;
 
-public class PasswordFragment extends Fragment {
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-	OnButtonClickListner mCallback;
+public class PasswordFragment extends Fragment implements RequestListener {
+
+    OnButtonClickListner mCallback;
+    EditText mEdit;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+                             Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_screen_signup_password, container, false);
-		Button next = (Button) rootView.findViewById(R.id.next);
-		next.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View view) {
-                    mCallback.onClickNextButton(3);
-                }
-			}); 
+        Button next = (Button) rootView.findViewById(R.id.next);
+        mEdit = (EditText) rootView.findViewById(R.id.password);
+        //TODO Change URL. This is dummy for testing. Make sure Data Connection is working.
+        RestClient.get("http://jsonplaceholder.typicode.com/posts/1", this);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCallback.onClickNextButton(3);
+            }
+        });
         return rootView;
     }
 
@@ -47,5 +59,31 @@ public class PasswordFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mCallback = null;
+    }
+
+    @Override
+    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+        //TODO Change it once Server is ready. This is dummy for testing purpose
+        try {
+            mEdit.setText(response.getString("title"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+
+    }
+
+    @Override
+    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+        mEdit.setText("Cannot Connect to Server");
+    }
+
+    @Override
+    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+        mEdit.setText("Cannot Connect to Server");
     }
 }
