aca62a473961310371110a7bae52a40e53a80697
dubsmania <itsmeprashantsingh@yahoo.com>
2015-08-01 19:55:28 +0530

    code for my dubs fragement

--- app/src/main/java/com/dubmania/dubsmania/misc/AddLanguageActivity.java
+++ app/src/main/java/com/dubmania/dubsmania/misc/AddLanguageActivity.java
@@ -5,15 +5,24 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.NumberPicker;
 
 import com.dubmania.dubsmania.R;
 
 public class AddLanguageActivity extends ActionBarActivity {
+    private NumberPicker picker;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_language);
+
+
+        picker = (NumberPicker) findViewById(R.id.language_picker);
+        picker.setMinValue(0);
+        picker.setMaxValue(2);
+        picker.setDisplayedValues(new String[]{"Belgium", "France", "United Kingdom" } );
     }
 
     @Override
