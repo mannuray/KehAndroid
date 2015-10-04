4e57d8891bf6026757d5cb86c93cc2ffd04d0293
dubsmania <itsmeprashantsingh@yahoo.com>
2015-10-05 01:25:56 +0530

    changes for selecting videoboard icon, change color depending on selected icon

--- app/src/main/java/com/dubmania/vidcraft/Adapters/ImageAdapter.java
+++ app/src/main/java/com/dubmania/vidcraft/Adapters/ImageAdapter.java
@@ -1,23 +1,30 @@
 package com.dubmania.vidcraft.Adapters;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.StateListDrawable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 
+import java.nio.channels.Selector;
 import java.util.ArrayList;
 import com.dubmania.vidcraft.R;
 
 public class ImageAdapter extends BaseAdapter {
     private Context mContext;
     private ArrayList<Integer> mThumbIds;
+    private ArrayList<String> mThumbColors;
 
 
     // Constructor
-    public ImageAdapter(Context c, ArrayList<Integer> myThumbIds) {
+    public ImageAdapter(Context c, ArrayList<Integer> myThumbIds, ArrayList<String> myColors) {
         mContext = c;
         mThumbIds = myThumbIds;
+        mThumbColors= myColors;
     }
 
     public int getCount() {
@@ -43,7 +50,12 @@ public class ImageAdapter extends BaseAdapter {
         {
             imageView = (ImageView) convertView;
         }
-        imageView.setBackground(mContext.getResources().getDrawable(R.drawable.add_video_board_selector));
+
+        GradientDrawable selected = (GradientDrawable) mContext.getResources().getDrawable(R.drawable.rectangular_background);
+        selected.setStroke(3, Color.parseColor(mThumbColors.get(position)));
+        StateListDrawable background = new StateListDrawable(); // mContext.getResources().getDrawable(R.drawable.add_video_board_selector);
+        background.addState(new int[]{android.R.attr.state_activated }, selected);
+        imageView.setBackground(background); //mContext.getResources().getDrawable(R.drawable.add_video_board_selector));
         imageView.setImageResource(mThumbIds.get(position));
         return imageView;
     }
